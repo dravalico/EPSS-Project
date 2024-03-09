2bb6f8ebce0105eefd0535ade0fa078a6c0941b8
dravalico <damianoravalico.uni@gmail.com>
2024-03-09 19:55:38 +0100

    Update

--- scripts/dataset_creation.py
+++ scripts/dataset_creation.py
@@ -5,13 +5,13 @@ import glob
 import json
 
 
-def init(threshold=0.7, storage_path='./datasets_new'):
+def init(threshold=0.7, storage_path='../datasets'):
     if not os.path.exists(storage_path):
         os.mkdir(storage_path)
     complete_dataset_path = os.path.join(storage_path, 'dataset.csv')
     if not os.path.exists(complete_dataset_path):
         path_epss_folder = '/share/smartdata/security/phishing/epss'
-        print('Starting reading from filesystem...')
+        print('Reading from filesystem...')
         all_files = glob.glob(os.path.join(path_epss_folder, '*', '*', '*.csv.gz'))
         dfs = []
         for file in all_files:
@@ -19,34 +19,38 @@ def init(threshold=0.7, storage_path='./datasets_new'):
                 df_temp = pd.read_csv(file_gz, comment='#')
                 df_temp['date'] = str(os.path.splitext(os.path.basename(file))[0]).replace('.csv', '')
                 dfs.append(df_temp)
+
+        # Save base datasets
         df_dataset = pd.concat(dfs, ignore_index=True)
         df_dataset['date'] = pd.to_datetime(df_dataset['date'], format='%Y-%m-%d')
         df_dataset = df_dataset.sort_values(by=['date'])
         df_dataset.to_csv(complete_dataset_path, index=False)
         df_dataset.to_pickle(os.path.join(storage_path, 'dataset.pkl'))
-        print('Complete dataset saved')
         date_new_model = '03-07-2023'
         dataset_epss_v2_path = os.path.join(storage_path, 'epss_v2.csv')
         df_epss_v2 = df_dataset[df_dataset['date'] < date_new_model]
         df_epss_v2.to_csv(dataset_epss_v2_path, index=False)
         df_epss_v2.to_pickle(os.path.join(storage_path, 'epss_v2.pkl'))
-        print('EPSS v2 dataset saved')
         dataset_epss_v3_path = os.path.join(storage_path, 'epss_v3.csv')
         df_epss_v3 = df_dataset[df_dataset['date'] >= date_new_model]
         df_epss_v3.to_csv(dataset_epss_v3_path, index=False)
         df_epss_v3.to_pickle(os.path.join(storage_path, 'epss_v3.pkl'))
-        print('EPSS v3 dataset saved')
-        filter_by_threshold(df_dataset, threshold, storage_path, 'dataset_highest')
-        filter_by_threshold(df_epss_v2, threshold, storage_path, 'epss_v2_highest')
-        filter_by_threshold(df_epss_v3, threshold, storage_path, 'epss_v3_highest')
-        print('Datasets with highest EPSS saved')
+
+        # Save dataset with CVE >T and <=T
+        save_by_filter_threshold(df_dataset, threshold, storage_path, 'dataset')
+        save_by_filter_threshold(df_epss_v2, threshold, storage_path, 'epss_v2')
+        save_by_filter_threshold(df_epss_v3, threshold, storage_path, 'epss_v3')
+        print('Completed')
 
 
-def filter_by_threshold(df, threshold, storage_path, name):
+def save_by_filter_threshold(df, threshold, path, name):
     highest_cve = set(df.groupby('cve').filter(lambda e: (e['epss'] > threshold).any())['cve'])
     df_highest = df[df['cve'].isin(highest_cve)]
-    df_highest.to_csv(os.path.join(storage_path, name + '.csv'), index=False)
-    df_highest.to_pickle(os.path.join(storage_path, name + '.pkl'))
+    df_highest.to_csv(os.path.join(path, name + '_highest.csv'), index=False)
+    df_highest.to_pickle(os.path.join(path, name + '_highest.pkl'))
+    df_lowest = df[~df['cve'].isin(highest_cve)]
+    df_lowest.to_csv(os.path.join(path, name + '_lowest.csv'), index=False)
+    df_lowest.to_pickle(os.path.join(path, name + '_lowest.pkl'))
 
 
 def download_dataset_and_extract_cve_pz():
@@ -65,16 +69,10 @@ def download_dataset_and_extract_cve_kev():
     return df_kev_sheet, cve_kev
 
 
-def save_or_read_filtered_df(path, df, cve_list):
-    if not os.path.exists(path):
-        df_res = df[df['cve'].isin(cve_list)]
-        df_res.to_csv(path, index=False)
-    else:
-        with open(path, 'r') as file_csv:
-            df_res = pd.read_csv(file_csv)
-        df_res['date'] = pd.to_datetime(df_res['date'])
-    print('df of CVEs from Google Project Zero ready')
-    return df_res
+def save_or_read_filtered_df(df, cve_list, path, name):
+    df_res = df[df['cve'].isin(cve_list)]
+    df_res.to_csv(os.path.join(path, name + '.csv'), index=False)
+    df_res.to_pickle(os.path.join(path, name + '.pkl'))
 
 
 if __name__ == '__main__':
